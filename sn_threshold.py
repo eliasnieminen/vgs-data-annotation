d95a487b9bd1a04e6ebdb69adc0e9e67a6e398c5
Elias Nieminen <elias.nieminen@tuni.fi>
2021-08-24 12:33:11 +0300

    Added saving to loop

--- sn_threshold.py
+++ sn_threshold.py
@@ -76,21 +76,22 @@ for file in Path(video_path).iterdir():
 
         timing_end = time.time()
 
+        time_id = format_2f(time.time())
+
         print(f"{str(file)} file took {format_2f(timing_end - timing_start)} seconds.")
+
+        print("Saving information so far...")
+        with open(f"misc/dist/speech_proportions_{time_id}.pickle",
+                  'wb') as f:
+            pickle.dump(speech_proportions, f)
+
+        with open(f"misc/dist/speech_proportions_per_file_{time_id}.pickle",
+                  'wb') as f:
+            pickle.dump(speech_proportions_per_file, f)
 
         count += 1
 
 print(f"Processed {count} videos.")
-
-time_id = format_2f(time.time())
-
-with open(f"misc/dist/speech_proportions_{time_id}.pickle",
-          'wb') as f:
-    pickle.dump(speech_proportions, f)
-
-with open(f"misc/dist/speech_proportions_per_file_{time_id}.pickle",
-          'wb') as f:
-    pickle.dump(speech_proportions_per_file, f)
 
 
 speech_proportions = np.array(speech_proportions)
